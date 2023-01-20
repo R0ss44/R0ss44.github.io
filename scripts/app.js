f6ce9822c9bc5885c9763b7335a5d4afd2f42404
Markus <mbrusgard@gmail.com>
2023-01-20 14:30:19 -0500

    ICE1 Upload

--- scripts/app.js
+++ scripts/app.js
@@ -10,6 +10,29 @@
             location.href = "about.html"
         });
     }
+
+    let MainContent = document.getElementsByTagName("main")[0];
+    let MainParagraph = document.createElement("p");
+
+    MainParagraph.setAttribute("id", "MainParagraph");
+    MainParagraph.setAttribute("class", "mt-3");
+    MainParagraph.textContent = "This is the main Paragraph!";
+
+    MainContent.appendChild(MainParagraph);
+
+    let FirstString = "This is";
+    let SecondString = `${FirstString} the Main Paragraph.`;
+    MainParagraph.textContent = SecondString;
+
+    //Step 6 - Create Article
+    let DocumentBody = document.body;
+
+    let Article = document.createElement("article");
+    let ArticleParagraph = `<p id="ArticleParagraph" class="mt-3">This is my article paragraph</p>`;
+    Article.setAttribute("class", "container");
+    Article.innerHTML = ArticleParagraph;
+    DocumentBody.appendChild(Article);
+
     function DisplayProductsPage(){
 
     }
