60c634d6a987f50e9dc45bfb8a38edb16407bf2a
Markus <mbrusgard@gmail.com>
2023-03-24 18:16:18 -0400

    ICE5 upload

---
+++ ICE5/hello.js
@@ -0,0 +1,25 @@
+"use strict";
+
+
+const helloMessage = "Hello World!";
+const goodByeMessage = "Good Bye!";
+
+
+function sayHello(){
+    console.log(helloMessage);
+}
+
+function sayGoodBye(){
+    console.log(goodByeMessage);
+}
+/**
+module.exports = {
+    sayHello : function() {
+        console.log(helloMessage);
+    },
+
+    sayGoodBye : function() {
+        console.log(goodByeMessage);
+    }
+};
+ **/
---
+++ ICE5/server.js
@@ -0,0 +1,33 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const fs_1 = __importDefault(require("fs"));
+const mime_types_1 = __importDefault(require("mime-types"));
+let lookup = mime_types_1.default.lookup;
+const port = process.env.PORT || 3000;
+const server = http_1.default.createServer((req, res) => {
+    let path = req.url;
+    console.log(__dirname);
+    console.log(path);
+    if (path === "/" || path === "/Home") {
+        path = "/index.html";
+    }
+    let mime_type = lookup(path.substring(1));
+    fs_1.default.readFile(__dirname + path, function (err, data) {
+        if (err) {
+            res.writeHead(404);
+            res.end("Error 404 - File Not Found " + err.message);
+            return;
+        }
+        res.setHeader("X-Content-Type-Options", "nosniff");
+        res.writeHead(200, { "Content-Type": mime_type });
+        res.end(data);
+    });
+});
+server.listen(port, () => {
+    console.log(`Server running at http://${port}:${port}/`);
+});
+//# sourceMappingURL=server.js.map
